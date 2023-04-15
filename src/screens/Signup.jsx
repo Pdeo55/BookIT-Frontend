0c762929b5d4175cb83401215c51fe829391fbd1
Aman Deshpande <amandeshpande392@gmail.com>
2023-04-15 23:14:38 +0530

    validations completed

--- src/screens/Signup.jsx
+++ src/screens/Signup.jsx
@@ -4,20 +4,63 @@ import BackgroundImage from "../assets/bg.png";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { register, reset } from "../features/auth/authSlice";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { toast } from "react-toastify";
+
+const phoneRegExp =
+  /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 
 const Signup = () => {
   const [userType, setUserType] = useState("");
-  const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-    phoneNo: "",
-    Organization: "",
-  });
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      phoneNo: "",
+      Organization: "",
+    },
+    validationSchema: Yup.object({
+      firstName: Yup.string()
+        .min(1, "First Name should not be empty")
+        .required("required"),
+      lastName: Yup.string()
+        .min(1, "last Name should not be empty")
+        .required("required"),
+      email: Yup.string().email("Invalid email address").required("required"),
+      password: Yup.string()
+        .required("required")
+        .min(5, "Must be 5 chars or more"),
+      phoneNo: Yup.string()
+        .required("required")
+        .matches(phoneRegExp, "Phone number is not valid")
+        .min(10, "too short")
+        .max(10, "too long"),
+      Organization: Yup.string()
+        .min(1, "Organization should not be empty")
+        .required("required"),
+    }),
+    onSubmit: (values) => {
+      if (userType.length === 0) {
+        toast.error("Choose the usertype");
+        return;
+      }
 
-  const { firstName, lastName, email, password, phoneNo, Organization } =
-    formData;
+      const userData = {
+        name: values.firstName + " " + values.lastName,
+        phoneNo: values.phoneNo,
+        Organization: values.Organization,
+        email: values.email,
+        password: values.password,
+        role: userType,
+      };
+
+      dispatch(register(userData));
+      console.log(userData);
+    },
+  });
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -35,31 +78,8 @@ const Signup = () => {
       navigate("/");
     }
 
-    dispatch(reset());
+    // dispatch(reset());
   }, [user, isError, isSuccess, message, navigate, dispatch]);
-
-  const onChange = (e) => {
-    setFormData((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-    }));
-  };
-
-  const onSubmitHandler = (e) => {
-    e.preventDefault();
-
-    const userData = {
-      name: firstName + " " + lastName,
-      phoneNo,
-      email,
-      password,
-      Organization,
-      userType,
-    };
-
-    dispatch(register(userData));
-    console.log(userData);
-  };
 
   return (
     <div className="w-full h-full flex">
@@ -89,35 +109,39 @@ const Signup = () => {
             </button>
           </div>
           <div className="w-full mt-10">
-            <form onSubmit={onSubmitHandler}>
+            <form onSubmit={formik.handleSubmit}>
               <div className="flex justify-between">
                 <div className="w-[45%]">
                   <label htmlFor="firstName" className="text-white">
                     First Name
                   </label>
                   <input
+                    id="firstName"
                     type="text"
                     required
                     placeholder="Enter first name"
                     className="w-full bg-black border-b border-[#FECA00] outline-none text-white"
-                    onChange={onChange}
-                    value={firstName}
-                    name="firstName"
+                    {...formik.getFieldProps("firstName")}
                   />
+                  {formik.touched.firstName && formik.errors.firstName ? (
+                    <p className="text-red-600">{formik.errors.firstName}</p>
+                  ) : null}
                 </div>
                 <div className="w-[45%]">
                   <label htmlFor="lastName" className="text-white">
                     Last Name
                   </label>
                   <input
+                    id="lastName"
                     type="text"
                     required
                     placeholder="Enter last name"
                     className="w-full bg-black border-b border-[#FECA00] outline-none text-white"
-                    onChange={onChange}
-                    value={lastName}
-                    name="lastName"
+                    {...formik.getFieldProps("lastName")}
                   />
+                  {formik.touched.lastName && formik.errors.lastName ? (
+                    <p className="text-red-600">{formik.errors.lastName}</p>
+                  ) : null}
                 </div>
               </div>
               <div className="mt-5">
@@ -125,14 +149,16 @@ const Signup = () => {
                   Organization
                 </label>
                 <input
+                  id="Organization"
                   type="text"
                   required
                   placeholder="Enter name of the organization"
                   className="w-full bg-black border-b border-[#FECA00] outline-none text-white"
-                  onChange={onChange}
-                  value={Organization}
-                  name="Organization"
+                  {...formik.getFieldProps("Organization")}
                 />
+                {formik.touched.Organization && formik.errors.Organization ? (
+                  <p className="text-red-600">{formik.errors.Organization}</p>
+                ) : null}
               </div>
               <div className="flex justify-between mt-5">
                 <div className="w-[45%]">
@@ -140,28 +166,32 @@ const Signup = () => {
                     Email Id
                   </label>
                   <input
+                    id="email"
                     type="email"
                     required
                     placeholder="Enter email Id"
                     className="w-full bg-black border-b border-[#FECA00] outline-none text-white"
-                    onChange={onChange}
-                    value={email}
-                    name="email"
+                    {...formik.getFieldProps("email")}
                   />
+                  {formik.touched.email && formik.errors.email ? (
+                    <p className="text-red-600">{formik.errors.email}</p>
+                  ) : null}
                 </div>
                 <div className="w-[45%]">
                   <label htmlFor="lastName" className="text-white">
                     Mobile Number
                   </label>
                   <input
-                    type="number"
+                    id="phoneNo"
+                    type="text"
                     required
                     placeholder="+91 1234567890"
                     className="w-full bg-black border-b border-[#FECA00] outline-none text-white"
-                    onChange={onChange}
-                    value={phoneNo}
-                    name="phoneNo"
+                    {...formik.getFieldProps("phoneNo")}
                   />
+                  {formik.touched.phoneNo && formik.errors.phoneNo ? (
+                    <p className="text-red-600">{formik.errors.phoneNo}</p>
+                  ) : null}
                 </div>
               </div>
               <div className="flex items-center mt-5">
@@ -202,17 +232,22 @@ const Signup = () => {
                   Create Password
                 </label>
                 <input
+                  id="password"
                   type="password"
                   required
                   placeholder="Enter password"
                   className="w-full bg-black border-b border-[#FECA00] outline-none text-white"
-                  onChange={onChange}
-                  value={password}
-                  name="password"
+                  {...formik.getFieldProps("password")}
                 />
+                {formik.touched.password && formik.errors.password ? (
+                  <p className="text-red-600">{formik.errors.password}</p>
+                ) : null}
               </div>
               <div className="w-full mt-10 flex flex-col items-center">
-                <button className="bg-[#EEE1FF] text-black rounded-lg font-bold px-8 py-2 hover:bg-[#a771ee]">
+                <button
+                  type="submit"
+                  className="bg-[#EEE1FF] text-black rounded-lg font-bold px-8 py-2 hover:bg-[#a771ee]"
+                >
                   SIGN UP
                 </button>
                 <p className="text-white mt-2 text-s">
